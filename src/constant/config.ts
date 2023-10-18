6afd175b61106401674314c178dc33ccd9c71c5d
sh-mimesis <shmelkov@mimesislabs.ai>
2023-10-18 22:30:28 +0100

    fixes

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Nomad Guides',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+    'A collection of guides for digital nomads, remote workers, and travelers.',
+  url: 'https://nomad-guild.vercel.app/',
 };
