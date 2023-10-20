551cc7ff2d9cd83f3ce88dd054d2e55d93df5d56
OSvol <somethingtrash@riseup.net>
2023-10-20 12:33:33 +0100

    Sismo verification

--- src/app/api/sismo/verify/route.ts
+++ src/app/api/sismo/verify/route.ts
@@ -1,68 +1,47 @@
-import {
-  AuthType,
-  ClaimType,
-  SismoConnect,
-  SismoConnectVerifiedResult,
-} from '@sismo-core/sismo-connect-server';
+import { SismoConnect } from '@sismo-core/sismo-connect-server';
 import { NextResponse } from 'next/server';
 
+import { supabase } from '@/lib/supabase';
+
 export async function POST(req: Request) {
-  const sismoConnectResponse = await req.json();
-  console.log(sismoConnectResponse);
-  // const {response: }
+  const { communityId, sismo } = await req.json();
 
-  const config = {
-    appId: process.env.NEXT_PUBLIC_SISMO_APP_ID || '',
-    vault: {
-      impersonate: [
-        'leo21.sismo.eth',
-        '0xA4C94A6091545e40fc9c3E0982AEc8942E282F38',
-        '0x1b9424ed517f7700e7368e34a9743295a225d889',
-        '0x82fbed074f62386ed43bb816f748e8817bf46ff7',
-        '0xc281bd4db5bf94f02a8525dca954db3895685700',
-        'github:leo21',
-        'twitter:leo21_eth',
-        'telegram:leo21',
-      ],
-    },
-  };
+  const { data, error } = await supabase
+    .from('community')
+    .select('sismo_group, url')
+    .eq('id', communityId);
 
-  const auths = [{ authType: AuthType.GITHUB }];
+  if (error) {
+    console.log(error);
+    return NextResponse.json({ error });
+  }
 
-  const claims = [
-    {
-      groupId: '0x1cde61966decb8600dfd0749bd371f12',
-      value: 15,
-      claimType: ClaimType.GTE,
-    },
-  ];
+  const url = data[0]?.url;
+  const sismo_group = data[0]?.sismo_group;
 
-  const sismoConnect = SismoConnect({
-    config,
-  });
+  if (sismo_group && url) {
+    const sismoConnect = SismoConnect({
+      config: {
+        appId: process.env.NEXT_PUBLIC_SISMO_APP_ID || '',
+      },
+    });
 
-  const result: SismoConnectVerifiedResult = await sismoConnect.verify(
-    sismoConnectResponse,
-    {
-      auths,
-      claims,
+    try {
+      await sismoConnect.verify(sismo, {
+        claims: [
+          {
+            groupId: sismo_group,
+          },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ result: 'Invalid proofs' });
     }
-  );
 
-  console.log(result.getUserIds(AuthType.VAULT));
-  // vault anonymous identifier = hash(vaultSecret, AppId)
-  // ['0x225c5b67c39778b40ef2528707c9fbdfed96f31b9a50826b95c2ac40e15e4c6b']
-  console.log(result.getUserIds(AuthType.GITHUB));
-  // [ '35774097' ] GitHub id of @dhadrien
-  console.log(result.getUserIds(AuthType.TWITTER));
-  // [ '2390703980' ] Twitter id of @dhadrien_
-  console.log(result.getUserIds(AuthType.EVM_ACCOUNT));
-  // [
-  //   '0x8ab1760889f26cbbf33a75fd2cf1696bfccdc9e6', // dhadrien.sismo.eth
-  //   '0xa4c94a6091545e40fc9c3e0982aec8942e282f38' // requested wallet auth
-  // ]
-  console.log(result.getUserIds(AuthType.TELEGRAM));
-  // [ '875608110' ] // Telegram id of @dhadrien
+    return NextResponse.json({ result: 'success', url });
+  }
 
-  return NextResponse.json(result);
+  console.log({ result: 'Incorrect communityId', communityId, sismo });
+  return NextResponse.json({ result: 'Incorrect communityId' });
 }
--- src/app/components/Sismo.tsx
+++ src/app/components/Sismo.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import {
-  AuthType,
-  ClaimType,
   SismoConnectButton,
   SismoConnectResponse,
 } from '@sismo-core/sismo-connect-react';
@@ -12,31 +10,10 @@ export default function Sismo() {
     <SismoConnectButton
       config={{
         appId: process.env.NEXT_PUBLIC_SISMO_APP_ID || '',
-        vault: {
-          // For development purposes insert the Data Sources that you want to impersonate here
-          // Never use this in production
-          impersonate: [
-            // EVM
-            'leo21.sismo.eth',
-            '0xA4C94A6091545e40fc9c3E0982AEc8942E282F38',
-            '0x1b9424ed517f7700e7368e34a9743295a225d889',
-            '0x82fbed074f62386ed43bb816f748e8817bf46ff7',
-            '0xc281bd4db5bf94f02a8525dca954db3895685700',
-            // Github
-            'github:leo21',
-            // Twitter
-            'twitter:leo21_eth',
-            // Telegram
-            'telegram:leo21',
-          ],
-        },
       }}
-      auths={[{ authType: AuthType.GITHUB }]}
       claims={[
         {
-          groupId: '0x1cde61966decb8600dfd0749bd371f12',
-          value: 15,
-          claimType: ClaimType.GTE,
+          groupId: '0xab882512e97b60cb2295a1c190c47569',
         },
       ]}
       onResponse={(response: SismoConnectResponse) => {
